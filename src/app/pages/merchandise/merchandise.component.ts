2a3bc2a5c5339f4975707b22d226ef7b83ae4ec6
Niklas Weinreich <Niklasweinreichhansen@hotmail.com>
2023-01-30 10:57:21 +0100

    30-01-2023

--- src/app/pages/merchandise/merchandise.component.ts
+++ src/app/pages/merchandise/merchandise.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-merchandise',
   templateUrl: './merchandise.component.html',
   styleUrls: ['./merchandise.component.css']
 })
-export class MerchandiseComponent {
+export class MerchandiseComponent implements OnInit {
+  sort = 'filtering';
 
-  constructor() {
+  constructor() {}
 
- }
+  ngOnInit(): void {
+
+  }
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+
+  }
+
 }
