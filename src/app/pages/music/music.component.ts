7dd2b156aeb5a9b52a0ebc276ec583218834d4db
Niklas Weinreich <Niklasweinreichhansen@hotmail.com>
2023-03-29 14:55:27 +0200

    29-03-2023

--- src/app/pages/music/music.component.ts
+++ src/app/pages/music/music.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
   styleUrls: ['./music.component.css']
 })
-export class MusicComponent {
+export class MusicComponent implements OnInit {
 
+
+  ngOnInit(): void {
+
+  }
+
+  setVolume(): void {
+
+  }
+
+  seekTo(): void {
+
+  }
+
+  prevTrack(): void {
+
+  }
+
+  nextTrack(): void {
+
+  }
+
+  playpauseTrack(): void {
+
+  }
 
 }
 
